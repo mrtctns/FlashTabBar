2aa313fccf39d78454af978ffcb76947ca802096
Mert Çetin <mrtctns22@gmail.com>
2023-07-04 14:35:33 +0300

    package update

--- Sources/FlashTabBar/FlashTabBar.swift
+++ Sources/FlashTabBar/FlashTabBar.swift
@@ -1,6 +1,39 @@
-public struct FlashTabBar {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
 
-    public init() {
+class FlashTabBarController: UITabBarController {
+    var tintColor: UIColor
+    var backgroundColor: UIColor
+    var tabbarItems: [TabbarItem]
+    
+    init(tintColor: UIColor, backgroundColor: UIColor, tabbarItems: [TabbarItem]) {
+        self.tintColor = tintColor
+        self.backgroundColor = backgroundColor
+        self.tabbarItems = tabbarItems
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure(tintColor: tintColor, backgroundColor: backgroundColor, tabbarItems: tabbarItems)
+    }
+    
+    func configure(tintColor: UIColor, backgroundColor: UIColor, tabbarItems: [TabbarItem]) {
+        self.tabBar.tintColor = tintColor
+        tabBar.backgroundColor = backgroundColor
+        
+        var viewControllers = [UIViewController]()
+        
+        tabbarItems.forEach { item in
+            guard let vc = item.VC else { return }
+            vc.tabBarItem = UITabBarItem(title: item.title, image: item.image, selectedImage: item.selectedImage)
+            let navController = UINavigationController(rootViewController: vc)
+            viewControllers.append(navController)
+        }
+        self.viewControllers = viewControllers
     }
 }
---
+++ Sources/FlashTabBar/TabbarItem.swift
@@ -0,0 +1,16 @@
+//
+//  TabbarItem.swift
+//  
+//
+//  Created by Mert Çetin on 4.07.2023.
+//
+
+import Foundation
+import UIKit
+
+struct TabbarItem {
+    let VC: UIViewController?
+    let title: String?
+    let image: UIImage?
+    let selectedImage: UIImage?
+}
